2ad8c4128a18abd0807dfc77176bf2b75839ebe2
puregorill <123246191+puregorill@users.noreply.github.com>
2023-07-06 16:23:08 +0200

    Add files via upload

--- MACRO HLA BASIC/MHLAB Internal/Graphic.asm
+++ MACRO HLA BASIC/MHLAB Internal/Graphic.asm
@@ -9,13 +9,13 @@
   sta $d018  
 }
 ;-------------------------------------------------------------------------------------------------
-; "position" | and | bits     | charmem address + current VIC bank
+; "position" | ora | bits     | charmem address + current VIC bank
 ;  in $d018  |     |          | hex        dez
 ;-------------------------------------------------------------------------------------------------
 ; 0          |  0  | xxxx000x | $0000      0
 ; 1          |  2  | xxxx001x | $0800      2048
-; 2          |  4  | xxxx010x | $1000      4096  ROM Image in Bank 0 & 2 (default: UpGfx CharSet)
-; 3          |  6  | xxxx011x | $1800      6144  ROM Image in Bank 0 & 2 (LoUp CharSet)
+; 2          |  4  | xxxx010x | $1000      4096  CHARROM image in Bank 0 & 2 (default: UpGfx CharSet)
+; 3          |  6  | xxxx011x | $1800      6144  CHARROM image in Bank 0 & 2 (LoUp CharSet)
 ; 4          |  8  | xxxx100x | $2000      8192
 ; 5          | 10  | xxxx101x | $2800     10240
 ; 6          | 12  | xxxx110x | $3000     12288
@@ -25,9 +25,6 @@
 
 !macro COPYCHARROM vic_bank, position {
 
-  ; _T_M_P_0_=(vic_bank*$4000)+(position*2048)
-  ; _T_M_P_1_=_T_M_P_0_+2047
-  
   +MEMORYCONFIG MEMORYCONFIG_BASIC_CHARROM_KERNAL, AREG, 1
 
   ; Number of bytes to copy + 256    
@@ -46,15 +43,15 @@
   +MEMORYCONFIG MEMORYCONFIG_DEFAULT_BASIC_IO_KERNAL, AREG, 1 
   
 }
-;---------------------------------------------
-; "vic_bank" | and | bits     | address
-;  in $dd00  |     |          | 
-;---------------------------------------------
-; 0          |     | xxxxxx11 | $0000-$3fff
-; 1          |     | xxxxxx10 | $4000-$7fff
-; 2          |     | xxxxxx01 | $8000-$bfff
-; 3          |     | xxxxxx00 | $c000-$ffff
-;---------------------------------------------
+;---------------------------------------
+; "vic_bank" | bits     | address
+;  in $dd00  |          |
+;---------------------------------------
+; 0          | xxxxxx11 | $0000-$3fff
+; 1          | xxxxxx10 | $4000-$7fff
+; 2          | xxxxxx01 | $8000-$bfff
+; 3          | xxxxxx00 | $c000-$ffff
+;---------------------------------------
 ; "position" is the same as in CHARMEM
 
 !macro VICBANK vic_bank {  
@@ -65,9 +62,9 @@
   } else if vic_bank=1 {
     ora #%00000010 
   } else if vic_bank=2 {
-    ora #%00000001 ; (with CHARMEM image)
+    ora #%00000001 ; (with CHARROM image)
   } else {
-    ora #%00000011 ; default bank 0 (with CHARMEM image)
+    ora #%00000011 ; default bank 0 (with CHARROM image)
 
   }
   sta $dd00
